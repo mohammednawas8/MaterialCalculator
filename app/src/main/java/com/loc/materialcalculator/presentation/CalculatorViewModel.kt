51c53be4776f569a2ddc09c9a28ef586a86d698a
Mohammad Nawas <mohammednawas8@gmail.com>
2025-01-21 21:47:09 +0200

    Created calculator ui

---
+++ app/src/main/java/com/loc/materialcalculator/presentation/CalculatorViewModel.kt
@@ -0,0 +1,21 @@
+package com.loc.materialcalculator.presentation
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.loc.materialcalculator.domain.CalculatorAction
+import com.loc.materialcalculator.domain.ExpressionWriter
+
+class CalculatorViewModel(
+    private val writer: ExpressionWriter = ExpressionWriter()
+): ViewModel() {
+
+    var expression by mutableStateOf("")
+        private set
+
+    fun onAction(action: CalculatorAction) {
+        writer.processAction(action)
+        expression = writer.expression
+    }
+}
